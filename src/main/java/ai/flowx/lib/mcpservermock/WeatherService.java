a7e54b9c353be49f5728a41646d1b4a52224ba43
calin-nicoara <calin.nicoara@flowx.ai>
2025-12-18 14:01:30 +0200

    changed tools

--- src/main/java/ai/flowx/lib/mcpservermock/WeatherService.java
+++ src/main/java/ai/flowx/lib/mcpservermock/WeatherService.java
@@ -18,27 +18,6 @@ public class WeatherService {
        return Map.of("temperature", "20C");
     }
 
-    @McpTool(description = "New tool baby with new name v2")
-    public Map<String, String> getNewToolBaby(
-            @McpToolParam(description = "This is different") String diff) {
-
-        return Map.of("all love", diff);
-    }
-
-    @McpTool(description = "Top of the line tools 2026")
-    public Map<String, String> bestTool2026(
-            @McpToolParam(description = "This is tool is best of the best") String top) {
-
-        return Map.of("no contest", top);
-    }
-
-    @McpTool(description = "Top of the line tools 2027 reloaded")
-    public Map<String, String> bestTool2027(
-            @McpToolParam(description = "This is tool is best of the best of the best") String top) {
-
-        return Map.of("no contest 2027", top);
-    }
-
   @McpTool(description = "gives life advice when somebody is down. It receives the problem as the input, and then the output"
       + "will be a very wise saying that will fix all the user's problmes.")
   public Map<String, String> giveLifeAdvice(
@@ -46,11 +25,4 @@ public class WeatherService {
 
     return Map.of("answer", "Don't worry, be happy!");
   }
-
-//    @McpTool(description = "New tool baby v2")
-//    public Map<String, String> getNewToolBabyv2(
-//            @McpToolParam(description = "Some sugar v2") String love) {
-//
-//        return Map.of("all love v2", love);
-//    }
 }
